a220845f5df6c335e2ddf900d92b6cb5715b144a
6666 <mask.monkey1994@gmail.com>
2025-04-02 16:29:13 +0700

    style: update message style

--- mfe/Chat/src/components/MessageBox.vue
+++ mfe/Chat/src/components/MessageBox.vue
@@ -15,18 +15,22 @@ const room_messages = computed(() => messages.get(rooms.selected!));
 </script>
 
 <template>
-  <section class="w-full h-full grid grid-rows-[1fr_auto]">
+  <section class="w-full h-full grid grid-rows-[1fr_auto] box-content">
     <div class="bg-gray-100 flex flex-col p-4 gap-0" data-testid="messages-box">
       <div
         v-for="(msg, index) in room_messages"
-        class="w-full flex"
+        class="w-full flex flex-col"
         :class="[
-          user.name === msg.user ? 'justify-end' : 'justify-start',
+          user.name === msg.user ? 'items-end' : 'items-start',
+          user.name === msg.user ? 'pl-8' : 'pr-8',
           room_messages[index - 1]?.user !== msg.user ? 'mt-2' : '',
         ]"
       >
-        <div class="bg-green-400 px-2 py-1">
-          <pre>{{ msg.value }} {{ msg.user }}</pre>
+        <span v-if="room_messages[index - 1]?.user !== msg.user">
+          [{{ msg.user }}]
+        </span>
+        <div class="bg-green-400 px-2 py-1 whitespace-pre-wrap">
+          <span>{{ msg.value }}</span>
         </div>
       </div>
     </div>
